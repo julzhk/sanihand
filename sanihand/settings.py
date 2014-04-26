32f01b904a0c43e8dc4297d9230d3dd93556df44
Julian Harley <julian.harley@gmail.com>
2014-04-26 16:21:09 +0100

    db settings

--- sanihand/settings.py
+++ sanihand/settings.py
@@ -60,6 +60,12 @@ WSGI_APPLICATION = 'sanihand.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
