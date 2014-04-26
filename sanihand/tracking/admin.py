22f3400cdb153f4e676d41b39748f1ac1389608a
Julian Harley <julian.harley@gmail.com>
2014-04-26 20:42:21 +0100

    add user model to admin

--- sanihand/tracking/admin.py
+++ sanihand/tracking/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from sanihand.tracking.models import Beacon, BeaconCheckin
+from sanihand.tracking.models import Beacon, BeaconCheckin, User
 admin.site.register(BeaconCheckin)
 admin.site.register(Beacon)
+admin.site.register(User)
