828e40d1abdae5413430ee21d0d19dcbba2a7357
Julian Harley <julian.harley@gmail.com>
2014-04-26 14:26:02 +0100

    sanihand

---
+++ sanihand/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sanihand project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sanihand.settings")
+from dj_static import Cling
+
+from django.core.wsgi import get_wsgi_application
+
+application = Cling(get_wsgi_application())
